a88e1afa28775ed6b5426d90010d871d582f9487
Idar Borlaug <idar.borlaug@bouvet.no>
2012-11-28 09:21:26 +0100

    formatering

--- src/test/java/no/ks/eventstore2/eventstore/testImplementations/NotificationSaga.java
+++ src/test/java/no/ks/eventstore2/eventstore/testImplementations/NotificationSaga.java
@@ -7,32 +7,33 @@ import no.ks.eventstore2.saga.Saga;
 import no.ks.eventstore2.saga.SagaRepository;
 
 @ListensTo(value = {
-        @EventIdBind(eventClass = LetterReceived.class, idProperty = "letterId"),
-        @EventIdBind(eventClass = NotificationSendt.class, idProperty = "letterId")
+		@EventIdBind(eventClass = LetterReceived.class, idProperty = "letterId"),
+		@EventIdBind(eventClass = NotificationSendt.class, idProperty = "letterId")
 }, aggregates = "notification")
 public class NotificationSaga extends Saga {
 
-    public NotificationSaga(String id, ActorRef commandDispatcher, SagaRepository repository) {
-        super(id, commandDispatcher, repository);
-    }
-
-    public void handleEvent(LetterReceived event){
-			if (getState() == STATE_INITIAL){
-				transitionState((byte)2);
-				commandDispatcher.tell("Send notification command", self());
-			}
+	public NotificationSaga(String id, ActorRef commandDispatcher, SagaRepository repository) {
+		super(id, commandDispatcher, repository);
 	}
-	public void handleEvent(NotificationSendt event){
-			if (getState() == 2){
-				transitionState((byte)3);
-				commandDispatcher.tell("Update logs", self());
-			}
 
-    }
+	public void handleEvent(LetterReceived event) {
+		if (getState() == STATE_INITIAL) {
+			transitionState((byte) 2);
+			commandDispatcher.tell("Send notification command", self());
+		}
+	}
+
+	public void handleEvent(NotificationSendt event) {
+		if (getState() == 2) {
+			transitionState((byte) 3);
+			commandDispatcher.tell("Update logs", self());
+		}
+
+	}
 
 	@Override
 	public void onReceive(Object o) throws Exception {
-		if ("BOOM!".equals(o)){
+		if ("BOOM!".equals(o)) {
 			throw new RuntimeException("BOOM!");
 		} else
 			super.onReceive(o);

41932be2b3e30bc559afc810c29e0699e19fd46c
Idar Borlaug <idar.borlaug@bouvet.no>
2012-12-10 14:30:41 +0100

    Fixed compile error

--- clustertest/build.sbt
+++ clustertest/build.sbt
@@ -6,6 +6,8 @@ scalaVersion := "2.10.0-RC2"
 
 resolvers += "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/"
 
+resolvers += "artifactory" at "http://jenkins.usrv.ubergenkom.no/artifactory/libs-snapshot-local"
+
 resolvers += "Local Maven Repository" at "file:///"+Path.userHome.absolutePath+"/.m2/repository"
 
 libraryDependencies += "no.ks" % "eventstore2" % "1.0-SNAPSHOT"
--- clustertest/src/main/java/akka/cluster/TestProjectionFactory.java
+++ clustertest/src/main/java/akka/cluster/TestProjectionFactory.java
@@ -2,12 +2,18 @@ package akka.cluster;
 
 import akka.actor.Actor;
 import akka.actor.ActorRef;
+import no.ks.eventstore2.projection.Projection;
 import no.ks.eventstore2.projection.ProjectionFactory;
 
 public class TestProjectionFactory extends ProjectionFactory {
 
     protected TestProjectionFactory(ActorRef eventstore) {
         super(eventstore);
+    }
+
+    @Override
+    public Class<? extends Projection> getProjectionClass() {
+        return TestProjection.class;
     }
 
     /**

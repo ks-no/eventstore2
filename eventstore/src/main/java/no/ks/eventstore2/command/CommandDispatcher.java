6de5de4ed8d389f1254df97e4b465c16ce110382
Idar Borlaug <idar.borlaug@bouvet.no>
2013-04-08 13:16:37 +0200

    PIA-1902 la til error log hvis det ikke finnes commandhandler for en command

--- eventstore/src/main/java/no/ks/eventstore2/command/CommandDispatcher.java
+++ eventstore/src/main/java/no/ks/eventstore2/command/CommandDispatcher.java
@@ -4,6 +4,8 @@ import akka.actor.ActorRef;
 import akka.actor.Props;
 import akka.actor.UntypedActor;
 import com.google.common.collect.ImmutableSet;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.HashMap;
 import java.util.List;
@@ -16,8 +18,9 @@ public class CommandDispatcher extends UntypedActor {
 	private Map<Class<? extends Command>, ActorRef> commandHandlers = new HashMap<Class<? extends Command>, ActorRef>();
 
     int remainingCommandHandlers = 0;
+    private static final Logger log = LoggerFactory.getLogger(CommandDispatcher.class);
 
-	public CommandDispatcher(ActorRef eventStore, List<CommandHandlerFactory> commandHandlerFactories) {
+    public CommandDispatcher(ActorRef eventStore, List<CommandHandlerFactory> commandHandlerFactories) {
 		this.eventStore = eventStore;
 		this.commandHandlerFactories = commandHandlerFactories;
 	}
@@ -42,8 +45,13 @@ public class CommandDispatcher extends UntypedActor {
 			ActorRef actorRef = commandHandlers.get(o.getClass());
             if(actorRef == null && remainingCommandHandlers > 0) {
                 self().tell(o, sender());
-            } else
-			    actorRef.tell(o, sender());
+            } else {
+                if(actorRef == null){
+                    log.error("Failed to find commandHandler for command {}", o);
+                } else {
+			        actorRef.tell(o, sender());
+                }
+            }
 		} else if(o instanceof ImmutableSet){
             ImmutableSet<Class<? extends Command>> handles = (ImmutableSet<Class<? extends Command>>) o;
             for (Class<? extends Command> clz : handles) {

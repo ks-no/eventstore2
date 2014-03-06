2387d9259e4e92966837efa28e24a561365cd7aa
Idar Borlaug <idar.borlaug@bouvet.no>
2014-03-06 15:17:44 +0100

    Ikke logg Success

--- eventstore/src/main/java/no/ks/eventstore2/DeadLetterLogger.java
+++ eventstore/src/main/java/no/ks/eventstore2/DeadLetterLogger.java
@@ -2,6 +2,7 @@ package no.ks.eventstore2;
 
 import akka.actor.DeadLetter;
 import akka.actor.UntypedActor;
+import no.ks.eventstore2.response.Success;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,6 +17,7 @@ public class DeadLetterLogger extends UntypedActor {
 
     @Override
     public void onReceive(Object o) throws Exception {
+        if(o instanceof DeadLetter && ((DeadLetter) o).message() instanceof Success) return;
         log.warn("Dead letter: {}", o);
     }
 

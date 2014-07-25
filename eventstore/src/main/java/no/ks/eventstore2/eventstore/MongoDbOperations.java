1870fbce7c54d9ce5e96c548ac086f4dd67e681b
Idar Borlaug <idar.borlaug@bouvet.no>
2014-07-25 10:34:31 +0200

    Kan velge retries og sleep time på mongodb operations

--- eventstore/src/main/java/no/ks/eventstore2/eventstore/MongoDbOperations.java
+++ eventstore/src/main/java/no/ks/eventstore2/eventstore/MongoDbOperations.java
@@ -10,11 +10,11 @@ public class MongoDbOperations {
 
     private static final Logger log = LoggerFactory.getLogger(MongoDbOperations.class);
 
-    public static <T> T doDbOperation(Callable<T> callable) {
+    public static <T> T doDbOperation(Callable<T> callable, int retries, int sleepms) {
         T result = null;
         boolean finished = false;
         int retrycount = 0;
-        while (!finished && retrycount < 100) {
+        while (!finished && retrycount < retries) {
             try {
                 retrycount++;
                 if (retrycount > 1)
@@ -22,13 +22,17 @@ public class MongoDbOperations {
                 result = callable.call();
                 finished = true;
             } catch (Exception e) {
-                log.debug("failed db operation", e);
+                log.info("failed db operation", e);
                 try {
-                    Thread.sleep(1000);
+                    Thread.sleep(sleepms);
                 } catch (InterruptedException e1) {
                 }
             }
         }
         return result;
+    }
+
+    public static <T> T doDbOperation(Callable<T> callable) {
+        return doDbOperation(callable, 50, 500);
     }
 }

a83be98f543f02028231f72246524a0bd9d8ec5c
Jan-Terje Sørensen <jtbsorensen@gmail.com>
2014-12-04 10:03:46 +0100

    Refactored H2 and MySQL journal storage to use same implementation of loadEventsAndHandle

--- eventstore/src/main/java/no/ks/eventstore2/eventstore/AbstractJournalStorage.java
+++ eventstore/src/main/java/no/ks/eventstore2/eventstore/AbstractJournalStorage.java
@@ -1,25 +1,52 @@
 package no.ks.eventstore2.eventstore;
 
 import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.io.Input;
 import com.esotericsoftware.kryo.io.Output;
 import no.ks.eventstore2.Event;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowCallbackHandler;
 
+import javax.sql.DataSource;
 import java.io.ByteArrayOutputStream;
+import java.sql.Blob;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public abstract class AbstractJournalStorage implements JournalStorage {
 
     private final ThreadLocal<Kryo> kryoThread = new ThreadLocal<>();
     private KryoClassRegistration kryoClassRegistration;
+    protected JdbcTemplate template;
 
-    public AbstractJournalStorage(KryoClassRegistration kryoClassRegistration) {
+    public AbstractJournalStorage(DataSource dataSource, KryoClassRegistration kryoClassRegistration) {
         this.kryoClassRegistration = kryoClassRegistration;
+        template = new JdbcTemplate(dataSource);
     }
 
     public abstract void saveEvent(Event event);
 
-    public abstract boolean loadEventsAndHandle(String aggregateType, HandleEvent handleEvent);
+    public boolean loadEventsAndHandle(String aggregateType, final HandleEvent handleEvent) {
+        return loadEventsAndHandle(aggregateType, handleEvent, "0");
+    }
 
-    public abstract boolean loadEventsAndHandle(String aggregateType, HandleEvent handleEvent, String fromKey);
+    @Override
+    public boolean loadEventsAndHandle(String aggregateType, final HandleEvent handleEvent, String fromKey) {
+        template.query("SELECT * FROM event WHERE aggregatetype = ? AND id >= ? ORDER BY id", new Object[]{aggregateType, Long.parseLong(fromKey)}, new RowCallbackHandler() {
+            @Override
+            public void processRow(ResultSet resultSet) throws SQLException {
+                if (resultSet.getInt("dataversion") == 2) {
+                    Blob blob = resultSet.getBlob("kryoeventdata");
+                    Input input = new Input(blob.getBinaryStream());
+                    Event event = (Event) getKryo().readClassAndObject(input);
+                    input.close();
+                    event.setJournalid(resultSet.getBigDecimal("id").toPlainString());
+                    handleEvent.handleEvent(event);
+                }
+            }
+        });
+        return true;
+    }
 
     public abstract void open();
 

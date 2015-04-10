bf40c30617105dce3483d2806fdf88acb93a3915
Jan-Terje Sørensen <jtbsorensen@gmail.com>
2015-04-10 07:46:38 +0200

    Fixed intellij error message in project that uses ES2

--- eventstore/src/main/java/no/ks/eventstore2/testkit/EventStoreTestKit.java
+++ eventstore/src/main/java/no/ks/eventstore2/testkit/EventStoreTestKit.java
@@ -9,6 +9,7 @@ import no.ks.eventstore2.eventstore.CompleteSubscriptionRegistered;
 import no.ks.eventstore2.saga.Saga;
 import no.ks.eventstore2.saga.SagaInMemoryRepository;
 import no.ks.eventstore2.util.IdUtil;
+import scala.concurrent.duration.Duration;
 
 public class EventStoreTestKit extends TestKit {
 
@@ -37,4 +38,8 @@ public class EventStoreTestKit extends TestKit {
         return (EventReceiver) TestActorRef.create(actorSystem, Props.create(EventReceiver.class), IdUtil.createUUID()).underlyingActor();
     }
 
+    @Override
+    public Duration awaitCond$default$3() {
+        return super.awaitCond$default$3();
+    }
 }

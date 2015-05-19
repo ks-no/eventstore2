17d13ee5e25aedd4d4a005bfde6b99081a32e921
Idar Borlaug <idar.borlaug@gmail.com>
2015-05-19 09:51:12 +0200

    Akka 2.3.11

--- eventstore/src/main/java/no/ks/eventstore2/projection/Call.java
+++ eventstore/src/main/java/no/ks/eventstore2/projection/Call.java
@@ -20,6 +20,26 @@ public class Call {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Call call = (Call) o;
+
+        if (method != null ? !method.equals(call.method) : call.method != null) return false;
+        // Probably incorrect - comparing Object[] arrays with Arrays.equals
+        return Arrays.equals(args, call.args);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = method != null ? method.hashCode() : 0;
+        result = 31 * result + (args != null ? Arrays.hashCode(args) : 0);
+        return result;
+    }
+
+    @Override
     public String toString() {
         return "Call{"
                 + "method='" + method + '\''

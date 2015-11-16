7d81518691e01c21a767637daa82b102c8bdbc8b
Idar Borlaug <idar.borlaug@gmail.com>
2015-11-16 12:02:53 +0100

    Download from jenkins

--- eventstore/src/test/java/no/ks/eventstore2/projection/MongoDbEventstore2TestKit.java
+++ eventstore/src/test/java/no/ks/eventstore2/projection/MongoDbEventstore2TestKit.java
@@ -2,13 +2,13 @@ package no.ks.eventstore2.projection;
 
 import com.mongodb.MongoClient;
 import com.mongodb.ServerAddress;
+import de.flapdoodle.embed.mongo.Command;
 import de.flapdoodle.embed.mongo.MongodExecutable;
 import de.flapdoodle.embed.mongo.MongodProcess;
 import de.flapdoodle.embed.mongo.MongodStarter;
-import de.flapdoodle.embed.mongo.config.IMongodConfig;
-import de.flapdoodle.embed.mongo.config.MongoCmdOptionsBuilder;
-import de.flapdoodle.embed.mongo.config.MongodConfigBuilder;
+import de.flapdoodle.embed.mongo.config.*;
 import de.flapdoodle.embed.mongo.distribution.Version;
+import de.flapdoodle.embed.process.config.IRuntimeConfig;
 import no.ks.eventstore2.Eventstore2TestKit;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -21,10 +21,22 @@ public class MongoDbEventstore2TestKit extends Eventstore2TestKit {
     private static MongodExecutable mongodExecutable = null;
     private static MongodProcess mongod = null;
     private static IMongodConfig mongodConfig;
-    static MongodStarter runtime = MongodStarter.getDefaultInstance();
+    static MongodStarter runtime;
 
     @BeforeClass
     public static void setUpMongoDb() throws Exception {
+        Command command = Command.MongoD;
+
+        IRuntimeConfig runtimeConfig = new RuntimeConfigBuilder()
+                .defaults(command)
+                .artifactStore(new ExtractedArtifactStoreBuilder()
+                        .defaults(command)
+                        .download(new DownloadConfigBuilder()
+                                .defaultsForCommand(command)
+                                .downloadPath("http://jenkins.usrv.ubergenkom.no/apps/")))
+                .build();
+        runtime = MongodStarter.getInstance(runtimeConfig);
+
         mongodConfig = new MongodConfigBuilder().version(Version.Main.V3_0).cmdOptions(new MongoCmdOptionsBuilder()
                 .useNoPrealloc(false)
                 .useSmallFiles(true)

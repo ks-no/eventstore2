35fc9be0a9214f9f68efdf1755580e3d7d5b7c85
Idar Borlaug <idar.borlaug@gmail.com>
2017-02-01 15:59:20 +0100

    Trenger kun en subscribe

--- eventstore/src/main/java/no/ks/eventstore2/projection/ProjectionProtobufSnapshot.java
+++ eventstore/src/main/java/no/ks/eventstore2/projection/ProjectionProtobufSnapshot.java
@@ -12,7 +12,6 @@ public abstract class ProjectionProtobufSnapshot extends ProjectionProtobuf {
     @Override
     public void preStart() {
         loadSnapshot();
-        subscribe();
         super.preStart();
     }
 
